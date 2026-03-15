c28bf0b39fbf812fdd3eec315a223637d898b1b7
Akshit Sharma <128965510+akshit7093@users.noreply.github.com>
2026-03-15 09:48:46 +0000

    added excalidraw file and modified first.js to include data types and console.table()

--- learning_js/first.js
+++ learning_js/first.js
@@ -1,1 +1,26 @@
+
+//simple print statement
 console.log("hello")
+
+
+
+//variable declaration
+var name = "John";
+var age = 30;
+var isStudent = true;
+
+//printing variables
+// console.log(name);
+// console.log(age);
+// console.log(isStudent);
+
+//data teypes
+
+var number = 42; //number
+var string = "Hello, World!"; //string
+var boolean = false; //boolean
+var array = [1, 2, 3, 4, 5]; //array
+var object = { name: "Alice", age: 25 }; //object
+
+//can print in form for of table
+console.table([number, string, boolean, array, object]); //displaying object in a table format
